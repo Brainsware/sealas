22de05d648f4f70f6998741cea9425c45a51ff3e
Daniel Khalil <d.khalil@brainsware.at>
2017-08-11 11:42:56 +0200

    chore: migration version

--- apps/sealas_api/priv/repo/migrations/20170810250000_create_invoice.exs
+++ apps/sealas_api/priv/repo/migrations/20170810250000_create_invoice.exs
@@ -3,8 +3,8 @@ defmodule SealasApi.Repo.Migrations.CreateInvoice do
 
   def change do
     create table(:invoice) do
-      add :contact_id,  references(:contact)
-      add :category_id, references(:invoice_category)
+      add :contact_id,  null: true, references(:contact)
+      add :category_id, null: true, references(:invoice_category)
 
       add :data,         :text, null: true
       add :contact_data, :text, null: true
