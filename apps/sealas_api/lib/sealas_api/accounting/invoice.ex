fc460c5a532a2bdde7719e32b0c9ff1fab2d0381
Daniel Khalil <d.khalil@brainsware.at>
2017-08-23 20:16:17 +0200

    refactor(api/invoices): merged model functionality into invoice schema

--- apps/sealas_api/lib/sealas_api/accounting/invoice.ex
+++ apps/sealas_api/lib/sealas_api/accounting/invoice.ex
@@ -1,12 +1,17 @@
 defmodule SealasApi.Accounting.Invoice do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query, warn: false
+  alias SealasApi.Repo
   alias SealasApi.Accounting.Invoice
-
+  alias SealasApi.Accounting.InvoiceCategory
+  alias SealasApi.Accounting.InvoicePaymentView
+  alias SealasApi.Accounting.ContactView
 
   schema "invoice" do
-    #field :contact_id,  references(:contact)
-    #field :category_id, references(:category)
+    belongs_to :contact,          ContactView
+    belongs_to :invoice_category, InvoiceCategory
+    has_many   :invoice_payment,  InvoicePaymentView
 
     field :company_data, :string
     field :contact_data, :string
@@ -21,5 +26,27 @@ defmodule SealasApi.Accounting.Invoice do
   def changeset(%Invoice{} = invoice, attrs) do
     invoice
     |> cast(attrs, [:data, :contact_data, :line_data, :log_data, :company_data, :type, :status])
+  end
+
+  def list do
+    Repo.all(Invoice)
+  end
+
+  def get!(id), do: Repo.get!(Invoice, id)
+
+  def create(attrs \\ %{}) do
+    %Invoice{}
+    |> Invoice.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update(%Invoice{} = invoice, attrs) do
+    invoice
+    |> Invoice.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete(%Invoice{} = invoice) do
+    Repo.delete(invoice)
   end
 end
--- apps/sealas_api/lib/sealas_api/controllers/invoice_controller.ex
+++ apps/sealas_api/lib/sealas_api/controllers/invoice_controller.ex
@@ -1,18 +1,17 @@
 defmodule SealasApi.InvoiceController do
   use SealasApi, :controller
 
-  alias SealasApi.Accounting
   alias SealasApi.Accounting.Invoice
 
   action_fallback SealasApi.FallbackController
 
   def index(conn, _params) do
-    invoice = Accounting.list_invoice()
+    invoice = Invoice.list()
     render(conn, "index.json", invoice: invoice)
   end
 
   def create(conn, %{"invoice" => invoice_params}) do
-    with {:ok, %Invoice{} = invoice} <- Accounting.create_invoice(invoice_params) do
+    with {:ok, %Invoice{} = invoice} <- Invoice.create(invoice_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", invoice_path(conn, :show, invoice))
@@ -21,21 +20,21 @@ defmodule SealasApi.InvoiceController do
   end
 
   def show(conn, %{"id" => id}) do
-    invoice = Accounting.get_invoice!(id)
+    invoice = Invoice.get!(id)
     render(conn, "show.json", invoice: invoice)
   end
 
   def update(conn, %{"id" => id, "invoice" => invoice_params}) do
-    invoice = Accounting.get_invoice!(id)
+    invoice = Invoice.get!(id)
 
-    with {:ok, %Invoice{} = invoice} <- Accounting.update_invoice(invoice, invoice_params) do
+    with {:ok, %Invoice{} = invoice} <- Invoice.update(invoice, invoice_params) do
       render(conn, "show.json", invoice: invoice)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    invoice = Accounting.get_invoice!(id)
-    with {:ok, %Invoice{}} <- Accounting.delete_invoice(invoice) do
+    invoice = Invoice.get!(id)
+    with {:ok, %Invoice{}} <- Invoice.delete(invoice) do
       send_resp(conn, :no_content, "")
     end
   end
