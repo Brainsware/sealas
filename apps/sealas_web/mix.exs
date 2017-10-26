1aeca00763af73ac4b97582d851c4fb5cf66e96e
Daniel Khalil <d.khalil@brainsware.at>
2017-10-26 15:26:15 +0200

    chore: removed ecto deps from sealas_web

--- apps/sealas_web/mix.exs
+++ apps/sealas_web/mix.exs
@@ -39,7 +39,6 @@ defmodule SealasWeb.Mixfile do
     [
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
@@ -52,6 +51,6 @@ defmodule SealasWeb.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+    #["test": ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
