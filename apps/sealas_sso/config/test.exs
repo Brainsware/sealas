bede32e3022d28244309ed46bbc2404ad4d34fee
Daniel Khalil <d.khalil@brainsware.at>
2017-10-27 14:28:43 +0200

    test(config): added test db config files

--- apps/sealas_sso/config/test.exs
+++ apps/sealas_sso/config/test.exs
@@ -3,9 +3,18 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :sealas_sso, SealasSso.Endpoint,
-  http: [port: 4001],
+  http: [port: 4002],
   server: false
 
 config :argon2_elixir,
   t_cost: 2,
   m_cost: 12
+
+config :sealas_sso, SealasSso.Repo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  adapter: Ecto.Adapters.Postgres,
+  username: "sealas",
+  password: "sealas",
+  database: "sealas",
+  hostname: "localhost",
+  pool_size: 10
