639283e32ec5bb57a86129131b288dec11d0a3e3
Daniel Khalil <d.khalil@brainsware.at>
2017-10-31 15:42:07 +0100

    feat(api/invoice): added queryable hashed fields "type" and "status" to invoice schema

--- apps/sealas_api/lib/sealas_api/accounting/invoice.ex
+++ apps/sealas_api/lib/sealas_api/accounting/invoice.ex
@@ -25,7 +25,7 @@ defmodule SealasApi.Accounting.Invoice do
   @doc false
   def changeset(%Invoice{} = invoice, attrs) do
     invoice
-    |> cast(attrs, [:data, :contact_data, :line_data, :log_data, :company_data, :type, :status])
+    |> cast(attrs, [:data, :contact_data, :line_data, :log_data, :company_data])
   end
 
   def list do
@@ -34,10 +34,25 @@ defmodule SealasApi.Accounting.Invoice do
 
   def get!(id), do: Repo.get!(Invoice, id)
 
+  defp get_by_query(type), do: "SELECT * FROM invoice WHERE #{type} = md5($1)::uuid"
+
+  def get_by!(type, value) do
+    {:ok, invoices} = Repo.query(get_by_query(type), [value])
+
+    invoices
+  end
+
   def create(attrs \\ %{}) do
-    %Invoice{}
-    |> Invoice.changeset(attrs)
-    |> Repo.insert()
+    # i = %Invoice{}
+    # |> Invoice.changeset(attrs)
+    # |> Repo.insert()
+
+    Repo.query("
+    INSERT INTO invoice
+    (data, company_data, line_data, log_data, contact_data, status, type)
+    VALUES ($1, $2, $3, $4, $5, md5($6)::uuid, md5($7)::uuid)
+    RETURNING id",
+    [attrs.data, attrs.company_data, attrs.line_data, attrs.log_data, attrs.contact_data, attrs.status, attrs.type])
   end
 
   def update(%Invoice{} = invoice, attrs) do
