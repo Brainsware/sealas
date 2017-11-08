54f0faeb5a2fb6f26a88ca8a2fd5f5baee461980
Daniel Khalil <d.khalil@brainsware.at>
2017-11-08 15:37:36 +0100

    feat(sso): added minimum request time to requests to SSO
    Fixes #9

--- apps/sealas_sso/lib/sealas_sso/router.ex
+++ apps/sealas_sso/lib/sealas_sso/router.ex
@@ -1,13 +1,31 @@
 defmodule SealasSso.Router do
   use SealasSso, :router
+  alias Plug.Conn
+
+  # Minimum request time in µs
+  @minimum_request_time 200_000
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :request_timer
   end
 
   scope "/", SealasSso do
     pipe_through :api
 
     resources "/user", UserController
+
+    resources "/auth", AuthController
+  end
+
+  def request_timer(conn, _options) do
+    time = Time.utc_now()
+
+    Conn.register_before_send(conn, fn conn ->
+      diff = Time.diff(Time.utc_now(), time, :microsecond)
+
+      if diff < @minimum_request_time, do: :timer.sleep round((@minimum_request_time - diff)/1000)
+      conn
+    end)
   end
 end
