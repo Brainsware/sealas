800c99b2e5b269f09b29a7bac355d31d0566ec5a
Daniel Khalil <d.khalil@brainsware.at>
2017-11-18 15:10:19 +0100

    test(api/ecto): uuid test

--- apps/sealas_api/test/sealas_api/ectohashindex_test.exs
+++ apps/sealas_api/test/sealas_api/ectohashindex_test.exs
@@ -1,11 +1,14 @@
 defmodule SealasApi.EctoHashIndexTest do
   use SealasApi.DataCase
-  import Ecto.Query, warn: false
 
   @test_invoice_uuid "c13bbe22-f8f6-55a0-47af-313e82edfbbd"
   @test_invoice_uuid_binary <<193, 59, 190, 34, 248, 246, 85, 160, 71, 175, 49, 62, 130, 237, 251, 189>>
 
   describe "casting custom ecto hash type" do
+    test "type is uuid" do
+      assert EctoHashIndex.type == Ecto.UUID
+    end
+    
     test "cast" do
       assert EctoHashIndex.cast("test_invoice") == {:ok, @test_invoice_uuid}
     end
