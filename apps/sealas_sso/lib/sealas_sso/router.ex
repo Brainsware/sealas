8ed4deb913daa13f6b4d80af76d6b4e6ea348876
Daniel Khalil <d.khalil@brainsware.at>
2017-11-23 13:57:17 +0100

    feat(sso/auth): added token auth check in router

--- apps/sealas_sso/lib/sealas_sso/router.ex
+++ apps/sealas_sso/lib/sealas_sso/router.ex
@@ -2,7 +2,7 @@ defmodule SealasSso.Router do
   use SealasSso, :router
   alias Plug.Conn
 
-  # Minimum request time in µs
+  @doc "Minimum request time in µs"
   @minimum_request_time 200_000
 
   pipeline :api do
@@ -10,12 +10,21 @@ defmodule SealasSso.Router do
     plug :request_timer
   end
 
+  pipeline :auth do
+    plug :check_token
+  end
+
   scope "/", SealasSso do
     pipe_through :api
 
-    resources "/user", UserController
-
     resources "/auth", AuthController
+  end
+
+  scope "/user", SealasSso do
+    pipe_through :api
+    pipe_through :auth
+
+    resources "/", UserController
   end
 
   def request_timer(conn, _options) do
@@ -27,5 +36,38 @@ defmodule SealasSso.Router do
       if diff < @minimum_request_time, do: :timer.sleep round((@minimum_request_time - diff)/1000)
       conn
     end)
+  end
+
+  def check_token(conn, _options) do
+    token = conn |> get_req_header("authorization")
+
+    case decrypt_token(List.first(token)) do
+      {:error} ->
+        conn
+          |> put_status(:unauthorized)
+          |> render(SealasSso.ErrorView, "401.json")
+          |> halt
+      _ ->
+        conn
+    end
+  end
+
+  def decrypt_token(auth_token) when is_binary(auth_token) do
+    auth_token = List.last(Regex.run(~r/(bearer\: )?(.+)/, auth_token))
+
+    key = Application.get_env(:sealas_sso, SealasSso.Endpoint)[:token_key]
+
+    jwk = %{"kty" => "oct", "k" => :base64url.encode(key)}
+
+    case JOSE.JWT.verify(jwk, {%{alg: :jose_jws_alg_hmac}, auth_token}) do
+      {true, token, _signature} ->
+        {:ok, token}
+      _ ->
+        {:error}
+    end
+  end
+
+  def decrypt_token(_) do
+    {:error}
   end
 end
