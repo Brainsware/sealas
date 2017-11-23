56513bd5c833957fe8149996f9c83ea100e7dd69
Daniel Khalil <d.khalil@brainsware.at>
2017-11-23 13:53:43 +0100

    feat(sso/views): added 401 and 403 error json

--- apps/sealas_sso/lib/sealas_sso/views/error_view.ex
+++ apps/sealas_sso/lib/sealas_sso/views/error_view.ex
@@ -9,6 +9,14 @@ defmodule SealasSso.ErrorView do
     "Internal server error"
   end
 
+  def render("401.json", _params) do
+    %{error: "auth fail"}
+  end
+
+  def render("403.json", _params) do
+    %{error: "auth fail"}
+  end
+
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
