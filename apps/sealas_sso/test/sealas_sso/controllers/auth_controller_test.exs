8ab429177c804b8dc86f050dea58b3f8928ead2b
Daniel Khalil <d.khalil@brainsware.at>
2017-11-27 22:46:09 +0100

    test(sso/auth): added prelim tests for TFA auth and registration

--- apps/sealas_sso/test/sealas_sso/controllers/auth_controller_test.exs
+++ apps/sealas_sso/test/sealas_sso/controllers/auth_controller_test.exs
@@ -3,11 +3,14 @@ defmodule SealasSso.AuthControllerTest do
 
   alias SealasSso.Repo
   alias SealasSso.Accounts.User
+  alias SealasSso.Accounts.UserTfa
 
-  @minimum_request_time 200_000
+  @minimum_request_time 190_000
 
   @create_attrs %{email: "some email", password: "some password"}
   @failed_login %{email: "some email", password: "wrong password"}
+
+  @create_tfa_attrs %{type: "yubikey", auth_key: "cccccccccccccccccccccccccccccccfilnhluinrjhl"}
 
   @registration_attrs %{email: "some@email.com", locale: "en"}
 
@@ -18,8 +21,9 @@ defmodule SealasSso.AuthControllerTest do
     user
   end
 
-  def fixture(:user, true) do
-    {:ok, user} = fixture(:user)
+  def fixture(:user, :with_tfa) do
+    user = fixture(:user)
+    {:ok, tfa}  = UserTfa.create(Map.put(@create_tfa_attrs, :user, user))
     user
   end
 
@@ -27,10 +31,21 @@ defmodule SealasSso.AuthControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  describe "index" do
+  describe "sso timing" do
+    test "minimum request time", %{conn: conn} do
+      time = Time.utc_now()
+
+      get conn, auth_path(conn, :index), @failed_login
+
+      diff = Time.diff(Time.utc_now(), time, :microsecond)
+      assert diff >= @minimum_request_time
+    end
+  end
+
+  describe "login" do
     setup [:create_user]
 
-    test "authenticate as a user", %{conn: conn} do
+    test "successful authentication as a user", %{conn: conn} do
       conn = get conn, auth_path(conn, :index), @create_attrs
       assert %{"auth" => auth_token} = json_response(conn, 201)
 
@@ -51,15 +66,6 @@ defmodule SealasSso.AuthControllerTest do
       assert json_response(conn, 200)
     end
 
-    test "minimum request time", %{conn: conn} do
-      time = Time.utc_now()
-
-      get conn, auth_path(conn, :index), @failed_login
-
-      diff = Time.diff(Time.utc_now(), time, :microsecond)
-      assert diff >= @minimum_request_time
-    end
-
     test "fail to authenticate", %{conn: conn} do
       conn = get conn, auth_path(conn, :index), @failed_login
       assert json_response(conn, 401) == %{"error" => "auth fail"}
@@ -72,8 +78,48 @@ defmodule SealasSso.AuthControllerTest do
     end
   end
 
+  describe "login with TFA" do
+    setup [:create_user_with_tfa]
+
+    test "successful authentication with TFA", %{conn: conn} do
+      conn = get conn, auth_path(conn, :index), @create_attrs
+      assert %{"tfa" => true, "code" => tfa_code} = json_response(conn, 201)
+
+      conn = get conn, auth_path(conn, :index), %{code: tfa_code, auth_key: "cccccccccccccccccccccccccccccccfilnhluinrjhl"}
+      assert json_response(conn, 201) == []
+    end
+
+    test "failed authentication with TFA", %{conn: conn} do
+      conn = get conn, auth_path(conn, :index), @create_attrs
+      assert %{"tfa" => true, "code" => tfa_code} = json_response(conn, 201)
+
+      conn = get conn, auth_path(conn, :index), %{code: tfa_code, auth_key: "wrong key!"}
+      assert json_response(conn, 401) == %{"error" => "auth fail"}
+    end
+  end
+
+  describe "registration" do
+    test "register as a new user", %{conn: conn} do
+      conn = post conn, user_path(conn, :create), user: @registration_attrs
+      assert %{"id" => id} = json_response(conn, 201)["data"]
+    end
+
+    test "register with an existing email", %{conn: conn} do
+      assert false
+    end
+
+    test "register with an existing unvalidated email", %{conn: conn} do
+      assert false
+    end
+  end
+
   defp create_user(_) do
     user = fixture(:user)
+    {:ok, user: user}
+  end
+
+  defp create_user_with_tfa(_) do
+    user = fixture(:user, :with_tfa)
     {:ok, user: user}
   end
 end
