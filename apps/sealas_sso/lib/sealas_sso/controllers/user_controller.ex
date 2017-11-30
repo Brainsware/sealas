a09c8f77adc02f39060a2f16f354612a4332a2cb
Daniel Khalil <d.khalil@brainsware.at>
2017-11-30 12:17:14 +0100

    chore: disabled user update

--- apps/sealas_sso/lib/sealas_sso/controllers/user_controller.ex
+++ apps/sealas_sso/lib/sealas_sso/controllers/user_controller.ex
@@ -10,11 +10,11 @@ defmodule SealasSso.UserController do
     render(conn, "index.json", %{users: []})
   end
 
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
-
-    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
-      render(conn, "show.json", user: user)
-    end
-  end
+  # def update(conn, %{"id" => id, "user" => user_params}) do
+  #   user = Accounts.get_user!(id)
+  #
+  #   with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+  #     render(conn, "show.json", user: user)
+  #   end
+  # end
 end
