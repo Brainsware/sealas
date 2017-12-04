38d7282ab1cdc56ca160b392a5cffd05bfa101e4
Daniel Khalil <d.khalil@brainsware.at>
2017-12-04 15:19:15 +0100

    doc(ecto): added custom ecto type docs

--- apps/sealas_api/lib/sealas_api/ectohashindex.ex
+++ apps/sealas_api/lib/sealas_api/ectohashindex.ex
@@ -1,7 +1,14 @@
 defmodule EctoHashIndex do
+  @moduledoc """
+  Ecto type for indexing hash values as UUIDs in Postgres
+  """
+
   @behaviour Ecto.Type
   def type, do: Ecto.UUID
 
+  @doc """
+  Convert string to md5 hash, then convert to UUID, unless the string is already a UUID.
+  """
   def cast(string) when is_binary(string) do
     case Regex.run(~r/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/, string) do
       nil -> Ecto.UUID.cast :crypto.hash(:md5, string)
