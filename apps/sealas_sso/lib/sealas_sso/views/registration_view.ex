95ad137531841a204d6f176ecef8e2ad52e3e339
Daniel Khalil <d.khalil@brainsware.at>
2018-01-10 19:27:43 +0100

    feat(sso/registration): view for auth code confirmation

--- apps/sealas_sso/lib/sealas_sso/views/registration_view.ex
+++ apps/sealas_sso/lib/sealas_sso/views/registration_view.ex
@@ -5,6 +5,10 @@ defmodule SealasSso.RegistrationView do
     %{error: error}
   end
 
+  def render("code.json", %{email: email}) do
+    %{email: email}
+  end
+
   def render("retry.json", %{activation_code: activation_code}) do
     %{error: "retry_validation", activation_code: activation_code}
   end
