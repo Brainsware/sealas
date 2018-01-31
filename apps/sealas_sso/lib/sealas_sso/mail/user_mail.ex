1801527c81921a9f5a0af1419ff45bb24459df0c
Daniel Khalil <d.khalil@brainsware.at>
2018-01-31 14:24:52 +0100

    feat(sso/mail): added base mailer and mail controller with assets

---
+++ apps/sealas_sso/lib/sealas_sso/mail/user_mail.ex
@@ -0,0 +1,24 @@
+defmodule SealasSso.UserMail do
+  use Phoenix.Swoosh, view: SealasSso.MailView, layout: {SealasSso.LayoutView, :mail}
+
+  defp prepare(user) do
+    logo     = Swoosh.Attachment.new("assets/sealas-logo-white-yellow.png", filename: "logo", content_type: "image/png", type: :inline)
+    twitter  = Swoosh.Attachment.new("assets/mail-facebook.png", filename: "facebook", content_type: "image/png", type: :inline)
+    facebook = Swoosh.Attachment.new("assets/mail-twitter.png", filename: "twitter", content_type: "image/png", type: :inline)
+    github   = Swoosh.Attachment.new("assets/mail-github.png", filename: "github", content_type: "image/png", type: :inline)
+
+    new()
+    |> to(user.email)
+    |> from({"Sealas Support", "support@sealas.at"})
+    |> attachment(logo)
+    |> attachment(twitter)
+    |> attachment(facebook)
+    |> attachment(github)
+  end
+
+  def verification(user) do
+    prepare(user)
+    |> subject("Is this reality?")
+    |> render_body(:verification, user)
+  end
+end
